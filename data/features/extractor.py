e920040eb41ba1852db00f899e7a97f32f894886
Sebastian Hurubaru <hurubaru@stanford.edu>
2019-11-17 20:14:55 +0000

    Create separate files for dev/train and test sets.

--- data/features/extractor.py
+++ data/features/extractor.py
@@ -3,17 +3,24 @@ from util.mongodb import *
 from util.config import *
 import argparse
 
+
+def dump_to_csv(values, columns, index_column_name, csv_file):
+    df = pd.DataFrame(values, columns=columns).set_index(index_column_name, drop=True)
+    df.to_csv(csv_file, index=True, header=True)
+
 if __name__ == "__main__":
 
     log = logging.getLogger('country_collection')
 
     parser = argparse.ArgumentParser()
     parser.add_argument("-ff", "--features_file", help="csv file to save the inputs(features)", type=str,
-                        default='/mnt/data/pycharm-projects/cs229/data/input/features_all.csv')
+                        default='/mnt/data/pycharm-projects/cs229/data/input/features.csv')
     parser.add_argument("-lf", "--labels_file", help="csv file to save the labels(outputs)", type=str,
-                        default='/mnt/data/pycharm-projects/cs229/data/input/labels_all.csv')
+                        default='/mnt/data/pycharm-projects/cs229/data/input/labels.csv')
     parser.add_argument("-t", "--type", help="which files to be produced: features/labels/both", type=str,
                         default='labels')
+    parser.add_argument("-tp", "--test_percentage", help="how much of the data to be saved as test", type=float,
+                        default='0.05')
     args = parser.parse_args()
 
     mongodb_connection = MongoDBWrapper('cs229')
@@ -28,6 +35,7 @@ if __name__ == "__main__":
 
     # Convert the countries to numpy array to ease some work
     countries_array = np.asarray(countries)
+    n_countries = np.shape(countries_array)[0]
 
     # Get all company numbers sorted in ascending order
     company_numbers = mongodb_connection.db.company.find({}, {'company_number': 1, '_id': 0})
@@ -39,9 +47,9 @@ if __name__ == "__main__":
         if args.type == 'both' or args.type == 'features':
 
             # Extracting the features
-            company_input = np.zeros(np.shape(countries))
+            company_input = np.zeros((2 * n_countries, ))
 
-            company_officer_appointments = mongodb_connection.db.officer_appointments.find({'appointed_to.company_number': company_number}, {'country_of_residence': 1, 'address.country': 1, '_id': 0})
+            company_officer_appointments = mongodb_connection.db.officer_appointments.find({'appointed_to.company_number': company_number}, {'country_of_residence': 1, 'address.country': 1, 'identification': 1, '_id': 0})
             if company_officer_appointments is not None:
                 for officer in company_officer_appointments:
 
@@ -54,8 +62,13 @@ if __name__ == "__main__":
                     elif 'address' in officer and 'country' in officer['address']:
                         officer_country = officer['address']['country']
 
-                    company_input[np.argwhere(countries_array == officer_country)] += 1
-
+                    # are we a corporate officer or a person
+                    if 'identification' in officer:
+                        # corporations are the 2nd half of the data
+                        company_input[np.argwhere(countries_array == officer_country) + n_countries] += 1
+                    else:
+                        # individuals are the 1st half of the data
+                        company_input[np.argwhere(countries_array == officer_country)] += 1
             else:
                 pass
                 # all values will be 0 if no officers
@@ -65,7 +78,7 @@ if __name__ == "__main__":
         if args.type == 'both' or args.type == 'labels':
 
             # Extracting the corresponding labels
-            company_label = np.zeros(np.shape(countries))
+            company_label = np.zeros(np.shape(countries_array))
             at_least_one_company_psc = False
 
             company_pscs = mongodb_connection.db.person_with_significant_control.find({'$text': {"$search": company_number}}, {'kind': 1, '_id': 0})
@@ -87,12 +100,20 @@ if __name__ == "__main__":
     log.info('Finished processing {} total of companies'.format(index))
 
     if args.type == 'both' or args.type == 'features':
-        input_df = pd.DataFrame(inputs, columns=['company_number'] + countries).set_index('company_number', drop=True)
-        input_df.to_csv(args.features_file, index=True, header=True)
+        n_examples = len(inputs)
+
+        dump_to_csv(inputs[slice(int((1 - args.test_percentage) * n_examples))], ['company_number'] + countries + countries,
+                    'company_number', args.features_file.replace('.csv', '_train_dev.csv'))
+        dump_to_csv(inputs[slice(int((1 - args.test_percentage) * n_examples), n_examples)], ['company_number'] + countries + countries,
+                    'company_number', args.features_file.replace('.csv', '_test.csv'))
 
     if args.type == 'both' or args.type == 'labels':
-        label_df = pd.DataFrame(labels, columns=['company_number', 'at_least_one_company_psc']).set_index('company_number', drop=True)
-        label_df.to_csv(args.labels_file, index=True, header=True)
-
+        n_examples = len(labels)
+        dump_to_csv(labels[slice(int((1 - args.test_percentage) * n_examples))],
+                    ['company_number', 'at_least_one_company_psc'], 'company_number',
+                    args.labels_file.replace('.csv', '_train_dev.csv'))
+        dump_to_csv(labels[slice(int((1 - args.test_percentage) * n_examples), n_examples)],
+                    ['company_number', 'at_least_one_company_psc'], 'company_number',
+                    args.labels_file.replace('.csv', '_test.csv'))
 
     log.info("Finished extracting features/labels out of the database")
