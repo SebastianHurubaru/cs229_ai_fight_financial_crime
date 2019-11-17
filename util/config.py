a048eabda26aa9ed0eae22af78baf86912e4c5d4
Sebastian Hurubaru <hurubaru@stanford.edu>
2019-11-17 20:15:20 +0000

    Improve logging capabilities

--- util/config.py
+++ util/config.py
@@ -1,17 +1,13 @@
 import logging
+import logging.config
+
 from logging import handlers
 
 import locale
 
 import pandas as pd
 
-logging.basicConfig(level=logging.INFO,
-                    format='%(asctime)s %(name)-12s %(levelname)-8s %(thread)s %(message)s',
-                    datefmt='%m-%d %H:%M:%S',
-                    handlers=[
-                        handlers.RotatingFileHandler("{0}/{1}.log".format('/mnt/data/pycharm-projects/cs229/logs', 'cs229'), maxBytes=(1048576*5), backupCount=10),
-                        logging.StreamHandler()
-                    ])
+logging.config.fileConfig('/mnt/data/pycharm-projects/cs229/util/logging.conf')
 
 MAX_REQ_FOR_KEY=600
 
