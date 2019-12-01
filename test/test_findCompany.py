a265a28f99472832be81a31719a7edbfba54d6e2
Sebastian Hurubaru <hurubaru@stanford.edu>
2019-12-01 14:14:23 +0000

    - test cases for the country detector - test case amendment for the findCompany method

--- test/test_findCompany.py
+++ test/test_findCompany.py
@@ -1,13 +1,15 @@
 from unittest import TestCase
-from util.mongodb import findCompany
+from util.mongodb import *
 
 class TestFindCompany(TestCase):
 
     def test_findCompany(self):
 
-        found1 = findCompany('Does not exist')
+        db = MongoDBWrapper('cs229')
+
+        found1 = db.findCompany('Does not exist')
         assert found1 == False, "Company 'Does not exist' should not be found"
 
-        found2 = findCompany('07891296')
+        found2 = db.findCompany('07891296')
         assert found2 == True, "Company '07891296' should be found"
 
